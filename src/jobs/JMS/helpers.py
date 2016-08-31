76c294e121d5861853e60685ff93357c039d7e96
David <davidbrownza@outlook.com>
2016-08-31 10:31:48 +0200

    Added run_ssh and run_impersonator functions to jobs to helpers.py

--- src/jobs/JMS/helpers.py
+++ src/jobs/JMS/helpers.py
@@ -1,4 +1,9 @@
+import os, subprocess, requests
+
+from django.conf import settings
+
 from resource_managers.objects import *
+from utilities.io.filesystem import File
 
 def parse_settings_list(settings_list):
     settings = []
@@ -36,3 +41,23 @@ def parse_queue_dict(queue_dict):
     queue = Queue(queue_dict["QueueName"], 
         parse_settings_sections_dict(queue_dict["SettingsSections"]))
     return queue
+
+
+def run_ssh(user, cmd):
+    cmd = "ssh %s@%s %s" % (user.username, settings.JMS_SETTINGS["user_processes"]["host"], cmd)
+    
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    
+    return out, err, process.returncode
+    
+
+def run_impersonator(user, cmd, sudo=False, expect="prompt"):
+    payload = "%s\n%s\n%s\n%s" % (user.filemanagersettings.ServerPass, cmd, expect, str(sudo))
+    #File.print_to_file("/tmp/files.txt", payload, permissions=0777)
+    
+    r = requests.post("http://127.0.0.1:%s/impersonate" % settings.JMS_SETTINGS["impersonator"]["port"], data=payload)
+    #File.print_to_file("/tmp/files.txt", payload, mode="a", permissions=0777)
+    
+    return r.text, None, r.status_code
+    
